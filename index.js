acd4d32a26d380b61e12553f691b8682276870ce
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-30 23:56:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function showBranches() {
   const branches = JSON.parse(this.responseText);
   const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`;
+  document.getElementById("details").innerHTML = branchesList;
 }
 
 function getBranches(element) {
