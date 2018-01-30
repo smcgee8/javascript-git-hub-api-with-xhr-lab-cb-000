c89811ebbfd6d6f769a0e034d74d6002960d4659
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-30 23:48:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function showCommits() {
   const commits = JSON.parse(this.responseText);
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
-  document.getElementById("commits").innerHTML = commitsList;
+  document.getElementById("details").innerHTML = commitsList;
 }
 
 function getCommits(element) {
