b864997ee309c80bb523adc8523ba156155758b8
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-30 23:27:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function getRepositories() {
 
   const req = new XMLHttpRequest();
   //req.addEventListener("load", showRepositories);
-  req.open("GET", "https://api.github.com/repos/" + username);
+  req.open("GET", "https://api.github.com/users/" + username + "/repos");
   req.send();
 }
